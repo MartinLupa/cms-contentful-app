cd64dc48bf579023a5627b59fa841c69e34f0aa0
Martin Lupa <marlupa@dfds.com>
2024-04-10 20:07:48 +0200

    flip to server side rendering

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 
 import Avatar from "../../../components/Avatar";
@@ -7,13 +8,13 @@ import CoverImage from "../../../components/CoverImage";
 import { Markdown } from "@/lib/markdown";
 import { getAllPostUrls, getAllPosts, getPostBySlug } from "@/lib/api";
 
-export async function generateStaticParams() {
-  const allPosts = await getAllPosts();
+// export async function generateStaticParams() {
+//   const allPosts = await getAllPosts();
 
-  return allPosts.map((post) => ({
-    slug: post.slug,
-  }));
-}
+//   return allPosts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
 
 export default async function PostPage({
   params,

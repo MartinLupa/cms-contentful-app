c4a36f044e6a76f3bc5ef1da91cbf8e881e0ac9c
Martin Lupa <marlupa@dfds.com>
2024-06-03 23:08:40 +0200

    fix: undefined environment variables

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ module.exports = {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
   },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  }
 };
